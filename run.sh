57ea8ffdb791de9ff472a24e003e78aed6fb9214
jackreece ejini <jackreeceejini@gmail.com>
2025-01-22 11:29:03 -0500

    add compile and build for kernel

--- run.sh
+++ run.sh
@@ -4,5 +4,15 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# new: Path to clang and compiler flags
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# new: Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
