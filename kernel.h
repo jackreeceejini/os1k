5b52b3b70293fd75e10f933d9125eaf0206fa16b
jackreece ejini <jackreeceejini@gmail.com>
2025-01-27 15:22:00 -0500

    add basic contex switching

--- kernel.h
+++ kernel.h
@@ -60,3 +60,4 @@ struct sbiret {
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
         while (1) {}                                                           \
     } while (0)
+
